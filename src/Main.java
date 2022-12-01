795285a6445e247340ee08a4f51f1970d5a11db4
DavidP1996erea <david.perea@iesnervion.es>
2022-12-01 20:13:15 +0100

    ,.ñ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,62 +9,73 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Se llena la lista con los equipos obtenidos por el método obtenerListaEquipos()
-        List<String> listaCompleta = obtenerListaEquipos();
+        List<Integer> numeros = new ArrayList<>();
 
-
-
-        menu();
+        obtenerListaEquipos();
 
 
     }
-
-
     /**
      * Método que lee el fichero con los nombres de los equipos y los guarda en una lista de String. Esta es lista
      * es retornada por el método.
      * @return
      */
-    public static List<String> obtenerListaEquipos(){
+    public static void obtenerListaEquipos(){
         BufferedReader br;
         Scanner sc;
-        List<String> listaCompleta = new ArrayList<>();
+        PreparedStatement ps;
+        String linea="";
+        Boolean dejarLeer=false;
         try {
-            br = new BufferedReader(new FileReader("C:\\Users\\dperea\\IdeaProjects\\fifa2\\equipos.txt"));
+            br = new BufferedReader(new FileReader("C:\\Users\\david\\IdeaProjects\\fifa2\\agenda.txt"));
             sc = new Scanner(br);
+/*
+            while(sc.hasNext() && !dejarLeer ){
 
-            while(sc.hasNext()){
-                listaCompleta.add(sc.nextLine());
+
+                linea= sc.nextLine();
+
+                if(linea.equals("##")){
+                    dejarLeer=true;
+                }else {
+                    System.out.println(linea);
+                }
+
+
             }
+*/
+
+     /*       while(sc.hasNextInt()){
+
+                linea= sc.nextLine();
+                if(dejarLeer){
+                    System.out.println(linea);
+                }
+                if(linea.equals("##")){
+
+                    dejarLeer=true;
+
+                }
+
+            }
+*/
+        while (sc.hasNext()){
+            linea = sc.nextLine();
+            if(sc.hasNextInt()){
+                System.out.println(linea );
+            }
+
+        }
+
+
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        return listaCompleta;
     }
 
 
 
-    public static int menu(){
-        int opcion=0;
 
-
-        opcionesMenu();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca una opcion");
-        opcion = sc.nextInt();
-
-        return  opcion;
-    }
-
-    public static void opcionesMenu(){
-
-        System.out.println("1. Iniciar octavos");
-        System.out.println("2. Iniciar cuartos");
-        System.out.println("3. Iniciar semifinales");
-        System.out.println("4. Iniciar la final");
-        System.out.println("5. Reiniciar");
-        System.out.println("6. Salir");
-    }
 }
